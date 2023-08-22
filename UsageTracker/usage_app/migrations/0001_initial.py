8cea4460f0aeb3271a6b60d8b35be04d6646956e
Aditi <asaki444@gmail.com>
2023-08-22 10:21:27 -0400

    removed migration tables that showed my changes to the table to a final table

--- UsageTracker/usage_app/migrations/0001_initial.py
+++ UsageTracker/usage_app/migrations/0001_initial.py
@@ -30,13 +30,13 @@ class Migration(migrations.Migration):
                 ('price_per_unit', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('units_consumed', models.IntegerField()),
                 ('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='usage_app.customer')),
+                ('processed', models.BooleanField(default=False) )
             ],
         ),
         migrations.CreateModel(
             name='AccumulatedUsage',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('accumulated_units',models.DecimalField(blank=True, decimal_places=2, null=True)),
                 ('accumulated_price', models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True)),
                 ('customer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='usage_app.customer')),
                 ('year',models.IntegerField(blank=True, null=True)),
